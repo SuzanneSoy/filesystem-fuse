f99cffbc894ec73c5cabf9455c45a60ef8bd6d3b
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-09-11 23:46:36 +0100

    tests pass

--- test.sh
+++ test.sh
@@ -2,24 +2,27 @@
 
 set -euET -o pipefail
 
+fusermount -u test/mnt || true
 rm test -fr
 mkdir test test/cache test/mnt
 cp -ai source test/source
 ./fs.py test/source test/cache test/mnt & pid=$!
 
 run_tests() {
+  sleep 2 # TODO: use foreground=False instead of & pid=$!
   touch test/mnt/touched
   echo 42 > test/mnt/written
+  echo 'Append' >> test/mnt/append
+  echo 'This is an overwrite' > test/mnt/overwrite
   cp -ai test/mnt test/actual_result
+  fusermount -u test/mnt
   diff -r test/actual_result expected_test_result
 }
 
 if run_tests; then
-  echo success
-  kill -KILL $pid
-  fusermount -u test_mnt
+  printf '\033[1;32mSuccess\033[m\n'
 else
-  echo failed
-  kill -KILL $pid
-  fusermount -u test_mnt
+  printf '\033[1;31mFailure\033[m\n'
+  fusermount -u test/mnt || kill -KILL $pid
+  exit 1
 fi
