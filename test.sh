3ecdc20ee4640a44fab39bae8b928c871d3785ff
Suzanne Soy <jsmaniac.github@suzanne.soy>
2023-09-11 22:13:55 +0100

    debug

--- test.sh
+++ test.sh
@@ -2,11 +2,24 @@
 
 set -euET -o pipefail
 
-rm test_cache test_source test_actual_result -fr
-mkdir -p test_cache test_mnt
-cp -ai source test_source
-./fs.py test_source test_cache test_mnt
-touch test_mnt/touched
-echo 42 > test_mnt/written
-cp -ai test_mnt test_actual_result
-diff -r test_actual_result test_expected_result
+rm test -fr
+mkdir test test/cache test/mnt
+cp -ai source test/source
+./fs.py test/source test/cache test/mnt & pid=$!
+
+run_tests() {
+  touch test/mnt/touched
+  echo 42 > test/mnt/written
+  cp -ai test/mnt test/actual_result
+  diff -r test/actual_result expected_test_result
+}
+
+if run_tests; then
+  echo success
+  kill -KILL $pid
+  fusermount -u test_mnt
+else
+  echo failed
+  kill -KILL $pid
+  fusermount -u test_mnt
+fi
